2a5f94c33e2b45cd8509aa082b23493b5ddd0c8f
David Gilbert <dagilbert333@gmail.com>
2018-09-28 03:05:18 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0 
   new_array = []
   array.each do |number|
-    new_array.push array[number ** 2]
+    new_array.push(array[number ** 2])
     count += 1
   end
   
